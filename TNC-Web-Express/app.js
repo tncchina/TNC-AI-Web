1624b69a3c5bc434561a739bc148f02e77265151
zhuoli liang <zhuoliseattle@gmail.com>
2017-11-15 22:42:38 -0800

    Upload photos to Azure storage bolbs

--- TNC-Web-Express/app.js
+++ TNC-Web-Express/app.js
@@ -6,16 +6,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
+var azure = require('azure-storage');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var uploads = require('./routes/upload');
-
+var photos = require('./routes/photos');
 
 var app = express();
-
-const fileUpload = require('express-fileupload');
-app.use(fileUpload());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,10 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/upload', uploads);
+app.use('/photos', photos);
 
 app.post('/upload', function (req, res) {
     if (!req.files)
@@ -39,14 +38,36 @@ app.post('/upload', function (req, res) {
 
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let sampleFile = req.files.sampleFile;
+    var blobSvc = azure.createBlobService();
+    var filePath = './photos/' + sampleFile.name;
+
+    if (!sampleFile.name.endsWith('.jpg'))
+        return res.status(400).send('Please upload photos with .jpg suffix only');
 
     // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv('./photos/' + sampleFile.name, function (err) {
+    sampleFile.mv(filePath, function (err) {
         if (err)
             return res.status(500).send(err);
 
-        res.render('upload', { title: sampleFile.name });
-        //res.send('File uploaded!');
+        res.send('File uploaded!' + req.files.sampleFile.name);
+
+        blobSvc.createContainerIfNotExists('mycontainer', { publicAccessLevel: 'blob' }, function (error, result, response) {
+            if (!error) {
+                // Container exists and allows
+                // anonymous read access to blob
+                // content and metadata within this container
+            }
+        });
+
+        blobSvc.createBlockBlobFromLocalFile('mycontainer', sampleFile.name, filePath, function (error, result, response) {
+            if (!error) {
+                // file uploaded
+                console.log("File uploaded to azure");
+            } else {
+                Console.log("Failed to upload to azure");
+            }
+        });
+
     });
 });
 
---
+++ TNC-Web-Express/routes/photos.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    fs.readdir('./downloads/', function (err, items) {
+        console.log(items);
+
+        for (var i = 0; i < items.length; i++) {
+            console.log(items[i]);
+        }
+
+        res.render('photos', {
+            title: 'photos',
+            imgurls: items
+        });
+    });
+});
+
+module.exports = router;
