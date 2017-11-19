60b2d7ad0739397c8b6a5e8ca6f67e705d08b601
zhuoli liang <zhuoliseattle@gmail.com>
2017-11-18 21:29:13 -0800

    Add photo controller

---
+++ TNC-Web-API/Controllers/PhotoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace TNC_Web_API.Controllers
+{
+    public class PhotoController : ApiController
+    {
+
+        [HttpPost]
+        public HttpResponseMessage Upload()
+        {
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+            if (httpRequest.Files.Count > 0)
+            {
+                var docfiles = new List<string>();
+                foreach (string file in httpRequest.Files)
+                {
+                    var postedFile = httpRequest.Files[file];
+                    var filePath = HttpContext.Current.Server.MapPath("~/" + postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+
+                    docfiles.Add(filePath);
+                }
+                result = Request.CreateResponse(HttpStatusCode.Created, docfiles);
+            }
+            else
+            {
+                result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return result;
+        }
+    }
+}
--- TNC-Web-API/Controllers/ProductsController.cs
+++ TNC-Web-API/Controllers/ProductsController.cs
@@ -5,23 +5,27 @@ using System.Net;
 using System.Net.Http;
 using TNC_Web_API.Models;
 using System.Web.Http;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace TNC_Web_API.Controllers
 {
     public class ProductsController : ApiController
     {
         Product[] products = new Product[]
-{
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
-};
+        {
+                    new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+                    new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+                    new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
 
+        [HttpGet]
         public IEnumerable<Product> GetAllProducts()
         {
             return products;
         }
 
+        [HttpGet]
         public IHttpActionResult GetProduct(int id)
         {
             var product = products.FirstOrDefault((p) => p.Id == id);
@@ -31,5 +35,6 @@ namespace TNC_Web_API.Controllers
             }
             return Ok(product);
         }
+
     }
 }
