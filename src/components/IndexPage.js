c6a8e662aa15cb81e09ad0e3a98c22bbe677467a
Peter Li <longhanli123@gmail.com>
2017-12-07 22:09:11 -0800

    First commit.

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,21 +3,58 @@
 import React from 'react';
 import AthletePreview from './AthletePreview';
 import athletes from '../data/athletes';
+const FileUploadWithPreview = require("file-upload-with-preview");
 
 export default class IndexPage extends React.Component {
-  render() {
-    return (
-    	<div onClick={this.function1}>
-    	<form ref='uploadForm' id='uploadForm' action='/upload' method='post' encType='multipart/form-data'>
-		    Select image to upload:
-		    <input type="file" name='sampleFile'/>
-		    <input type="submit" value='Upload!' name="submit" />
-		</form>
-		</div>
-    );
-  }
 
-  function1() {
-  	consol.log("ddd");
-  }
+	render() {
+
+		return (
+			<div className="custom-file-container" data-upload-id="myFirstImage">
+				<div className="demo-upload-container">
+					<div className="custom-file-container" data-upload-id="myFirstImage">
+						<label>Upload (Single File) <a href="javascript:void(0)" className="custom-file-container__image-clear" title="Clear Image">x</a></label>
+						<label className="custom-file-container__custom-file" >
+							<input type="file" className="custom-file-container__custom-file__custom-file-input" accept="image/*" />>
+									<input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+							<span className="custom-file-container__custom-file__custom-file-control"></span>
+						</label>
+						<div className="custom-file-container__image-preview"></div>
+					</div>
+
+					<div className="custom-file-container" data-upload-id="mySecondImage">
+						<label>Upload (Allow Multiple) <a href="javascript:void(0)" className="custom-file-container__image-clear" title="Clear Image">x</a></label>
+						<label className="custom-file-container__custom-file" >
+							<input type="file" class="custom-file-container__custom-file__custom-file-input" accept="*" multiple />>
+									<input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+							<span className="custom-file-container__custom-file__custom-file-control"></span>
+						</label>
+						<div className="custom-file-container__image-preview"></div>
+					</div>
+				</div>
+
+				<div class="demo-info-container">
+					<div>
+						<a href="javascript:void(0)" className="upload-info-button upload-info-button--first">Get first file info</a>
+						<a href="javascript:void(0)" className="upload-info-button upload-info-button--second">Get second file-group info</a>
+						<span>Output will be in the console.</span>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		var firstUpload = new FileUploadWithPreview('myFirstImage')
+		var firstUploadInfoButton = document.querySelector('.upload-info-button--first');
+		firstUploadInfoButton.addEventListener('click', function(){
+			console.log('First upload:', firstUpload, firstUpload.cachedFileArray);
+		});
+		//Second upload
+		var secondUpload = new FileUploadWithPreview('mySecondImage')
+		var secondUploadInfoButton = document.querySelector('.upload-info-button--second');
+		secondUploadInfoButton.addEventListener('click', function(){
+			console.log('Second upload:', secondUpload, secondUpload.cachedFileArray)
+		});
+	}
 }
