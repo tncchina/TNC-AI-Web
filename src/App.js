42aec60d9b8d8dd7ac4cfa8fd08923c477ddf295
zhuoliseattle@gmail.com <seattle8482617>
2018-01-19 15:54:34 -0800

    new

--- src/App.js
+++ src/App.js
@@ -7,44 +7,23 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { pictures: [] };
-    this.onDrop = this.onDrop.bind(this);
-}
+     this.state = { pictures: [] };
+     this.onDrop = this.onDrop.bind(this);
+  }
 
-onDrop(picture) {
-    this.setState({
-        pictures: this.state.pictures.concat(picture),
-    });
-    fetch('http://localhost:49492/api/storage/photoUpload', {
-      method: 'POST',
-      headers: {
-        'Access-Control-Allow-Origin':'*',
-        'Access-Control-Allow-Methods':'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token',
-        'Content-Type': 'multipart/form-data',
-        'boundary':'---------------------------9051914041544843365972754266',
-        'MIME-Version':'1.0'
-      },
-      body: `-----------------------------9051914041544843365972754266
-      Content-Disposition: form-data; name="text/plain"
-      
-      text default
-      -----------------------------9051914041544843365972754266
-      Content-Disposition: form-data; name="file1"; filename="a.txt"
-      Content-Type: text/plain
-      
-      Content of a.txt.
-      
-      -----------------------------9051914041544843365972754266
-      Content-Disposition: form-data; name="file2"; filename="a.html"
-      Content-Type: text/html
-      
-      <!DOCTYPE html><title>Content of a.html.</title>
-      
-      -----------------------------9051914041544843365972754266--`
-    })
-    console.log("Done");
-}
+  onDrop(pictures) {
+      var picture = pictures[0]
+      this.setState({
+          pictures: this.state.pictures.concat(picture),
+      });
+
+      var formData = new FormData();
+      formData.append(picture.name, picture);
+      fetch('http://localhost:49492/api/storage/photoUpload', {
+        method: 'POST',
+        body: formData
+      })
+  }
 
   render() {
     return (
@@ -54,15 +33,17 @@ onDrop(picture) {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <ImageUploader
-          withIcon={true}
-          buttonText='Choose images'
-          onChange={this.onDrop}
-          imgExtension={['.jpg', '.gif', '.png', '.gif']}
-          maxFileSize={5242880}
-        />
+        withIcon={true}
+        buttonText='Choose images'
+        onChange={this.onDrop}
+        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+        maxFileSize={5242880}
+    />
       </div>
     );
   }
 }
 
 export default App;
+
+
